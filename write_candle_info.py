0f8e9eb53b081ff8e0a0fe5d00cbce9e01d39080
Cyrus <67963698+CyrusHasanvand@users.noreply.github.com>
2025-07-01 02:47:24 +0330

    Create write_candle_info.py

---
+++ write_candle_info.py
@@ -0,0 +1,23 @@
+# Candle price information
+High = 6010
+Low = 6003
+Open = 6004
+Close = 6008
+
+# Attempt to write the information into a file using try-except
+while True:
+    try:
+        with open("Candle Information.txt", "w") as f:
+            f.write(
+                "\nCandle Info:\n"
+                "High Price: %.2f\n"
+                "Low Price: %.2f\n"
+                "Open Price: %.2f\n"
+                "Close Price: %.2f\n" % (High, Low, Open, Close)
+            )
+        print("File written successfully.")
+        break  # Exit loop if successful
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        print("The process will check again.\n")
